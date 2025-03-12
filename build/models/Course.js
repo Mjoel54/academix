902eec395cbbf7af54c32a35e86901c53a4c771f
Mitch <mitchjoelklein@hotmail.com>
2025-03-12 16:02:37 +1100

    get course route

--- build/models/Course.js
+++ build/models/Course.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-const roomSchema = new mongoose_1.Schema({
+const courseSchema = new mongoose_1.Schema({
     title: { type: String, required: true, trim: true, maxlength: 50 },
     assignments: { type: [String], required: false },
     students: { type: [String], required: false },
     teachers: { type: [String], required: false },
 });
 // To use the schema definition, we need to convert roomSchema into a Model we can work with.
-const Course = (0, mongoose_1.model)("Course", roomSchema);
+const Course = (0, mongoose_1.model)("Course", courseSchema);
 exports.default = Course;
--- build/server.js
+++ build/server.js
@@ -17,6 +17,7 @@ const cors_1 = __importDefault(require("cors"));
 const connection_1 = require("./db/connection");
 const connection_2 = __importDefault(require("./db/connection"));
 const mongodb_1 = require("mongodb");
+const Course_1 = __importDefault(require("./models/Course"));
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const app = (0, express_1.default)();
@@ -25,7 +26,7 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     try {
         res.json({
-            msg: "It's workin'!",
+            msg: "It's working!",
         });
     }
     catch (x) {
@@ -33,7 +34,17 @@ app.get("/", (req, res) => {
         res.json({ error: x });
     }
 });
-app.get("/courses/all", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/courses", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const courses = yield Course_1.default.find();
+        res.json({ docs: courses });
+    }
+    catch (error) {
+        console.error("Error fetching courses:", error);
+        res.json({ error });
+    }
+}));
+app.get("/item/all", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const allItems = yield connection_1.db.collection("courses").find().toArray();
         res.json({ docs: allItems });
@@ -59,6 +70,6 @@ app.get("/item/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* (
 }));
 app.listen(process.env.PORT || PORT, () => {
     console.log("And we're rolling! 🎥");
-    console.log(`Use GraphQL at http://localhost:${PORT}`);
+    console.log(`API server is live at http://localhost:${PORT}`);
 });
 exports.default = app;
