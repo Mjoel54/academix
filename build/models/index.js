e371460f28478a656ed011febfd56811dcfa1dd3
Mitch <mitchjoelklein@hotmail.com>
2025-03-23 12:03:54 +1100

    deleted build dir

--- build/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Course = void 0;
-const Course_1 = __importDefault(require("./Course"));
-exports.Course = Course_1.default;
