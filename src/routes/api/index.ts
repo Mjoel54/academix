cacc2c33cbf785e9bc77c0cf009739a161ebed37
Mitch <mitchjoelklein@hotmail.com>
2025-03-31 16:37:23 +1100

    router uses /terms

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import courseRoutes from "./courseRoutes";
 import userRoutes from "./userRoutes";
+import termRoutes from "./termRoutes";
 
 const router = Router();
 
 router.use("/courses", courseRoutes);
 router.use("/users", userRoutes);
+router.use("/terms", termRoutes);
 
 export default router;
